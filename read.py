43c44ba1e7af27f24cae2b35a32daf271e1ff62d
Hsin Yu <winter0462@gmail.com>
2021-10-03 18:26:51 +0800

    bug fix and calc avg length of reviews

--- read.py
+++ read.py
@@ -4,11 +4,11 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0
-		print(len(data))
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完成！總共有', len(data), '筆評論')
 
-print(len(data))
-
-print(data[0])
-print('--------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('評論的平均長度為:', sum_len/len(data), '個字')
